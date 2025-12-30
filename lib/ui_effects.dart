2e80abd21a91d49267cdd0148ffb914de8e3093f
Yoeri <yoeriz123@gmail.com>
2025-12-30 20:49:13 +0100

    added inspectable handler to exports

--- lib/ui_effects.dart
+++ lib/ui_effects.dart
@@ -4,6 +4,7 @@ export 'src/center.dart';
 export 'src/event.dart';
 export 'src/context_extensions.dart';
 export 'src/handlers/element_handler.dart';
+export 'src/handlers/inspectable_handler.dart';
 
 export 'src/toast/toast.dart';
 export 'src/toast/toast_theme.dart';
